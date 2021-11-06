f78de8ced8f9b1d30308a9b1e3a6948070e5b6ac
plveir <featveir@gmail.com>
2021-11-07 06:10:57 +0900

    add USEFUL reply methods

---
+++ src/discord/util.ts
@@ -0,0 +1,13 @@
+import { CacheType, CommandInteraction, InteractionReplyOptions, MessagePayload } from "discord.js"
+
+export async function usefulReplyOrFollowUp (interaction:  CommandInteraction<CacheType>, options: string | MessagePayload | InteractionReplyOptions): Promise<void> {
+    if (interaction.deferred && interaction.replied) {
+        await interaction.followUp(options).catch(console.warn);
+    } else if (interaction.deferred) {
+        await interaction.editReply(options).catch(console.warn);
+    } else if (interaction.replied) {
+        await interaction.followUp(options).catch(console.warn); 
+    } else {
+        await interaction.reply(options).catch(console.warn);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { Track } from './music/track/track';
 import { generateDependencyReport } from '@discordjs/voice';
 import { Register } from './commands/register';
 import { Url2Enqueue } from './music/url2enqueue';
+import { usefulReplyOrFollowUp } from './discord/util';
 
 console.log(generateDependencyReport());
 const options: ClientOptions = {
@@ -50,7 +51,7 @@ client.on('interactionCreate', async (interaction: Interaction) => {
 		
 		if (!subscription) {
 			// If there is no subscription, tell the user they need to join a channel.
-			await interaction.followUp('Join a voice channel and then try that again!');
+			await usefulReplyOrFollowUp(interaction, 'Join a voice channel and then try that again!');
 			return;
 		}
 
@@ -59,8 +60,7 @@ client.on('interactionCreate', async (interaction: Interaction) => {
 			await entersState(subscription.voiceConnection, VoiceConnectionStatus.Ready, 20e3);
 		} catch (error) {
 			console.warn(error);
-			await interaction.followUp('Failed to join voice channel within 20 seconds, please try again later!');
-			interaction.deferred
+			await usefulReplyOrFollowUp(interaction, 'Failed to join voice channel within 20 seconds, please try again later!');
 			return;
 		}
 
@@ -71,17 +71,20 @@ client.on('interactionCreate', async (interaction: Interaction) => {
 				interaction,
 				url,
 				{
-					onStart(title: string){interaction.followUp({ content: `再生中: **${title}**` }).catch(console.warn);},
+					async onStart(title: string){
+						await usefulReplyOrFollowUp(interaction, ({ content: `再生中: **${title}**` }));
+					},
 					// eslint-disable-next-line @typescript-eslint/no-empty-function
 					onFinish(){},
-					onError(error) {
+					async onError(error) {
 						console.warn(error);
-						interaction.followUp({ content: `Error: ${error.message}`, ephemeral: true }).catch(console.warn);
-				},
-			});
+						await usefulReplyOrFollowUp(interaction, ({ content: `Error: ${error.message}`, ephemeral: true }));
+					},
+				}
+			);
 		} catch (error) {
-			console.warn(error);
-			await interaction.followUp(error);
+			console.warn("url2enqueue error:", error);
+			await usefulReplyOrFollowUp(interaction, error);
 		}
 	} else if (interaction.commandName === 'skip') {
 		if (subscription) {
@@ -89,9 +92,9 @@ client.on('interactionCreate', async (interaction: Interaction) => {
 			// listener defined in music/subscription.ts, transitions into the Idle state mean the next track from the queue
 			// will be loaded and played.
 			subscription.audioPlayer.stop();
-			await interaction.reply('スキップ');
+			await usefulReplyOrFollowUp(interaction, 'スキップ');
 		} else {
-			await interaction.reply('何も再生してないよ？');
+			await usefulReplyOrFollowUp(interaction, '何も再生してないよ？');
 		}
 	} else if (interaction.commandName === 'queue') {
 		// Print out the current queue, including up to the next 5 tracks to be played.
@@ -106,36 +109,36 @@ client.on('interactionCreate', async (interaction: Interaction) => {
 				.map((track, index) => `${index + 1}) ${track.title}`)
 				.join('\n');
 
-			await interaction.reply(`${current}\n\n${queue}`);
+			await usefulReplyOrFollowUp(interaction, `${current}\n\n${queue}`);
 		} else {
-			await interaction.reply({ content: '何も再生してないよ？', ephemeral: true });
+			await usefulReplyOrFollowUp(interaction, { content: '何も再生してないよ？', ephemeral: true });
 		}
 	} else if (interaction.commandName === 'pause') {
 		if (subscription) {
 			subscription.audioPlayer.pause();
-			await interaction.reply({ content: `一時停止`, ephemeral: true });
+			await usefulReplyOrFollowUp(interaction, { content: `一時停止`, ephemeral: true });
 		} else {
-			await interaction.reply('何も再生してないよ？');
+			await usefulReplyOrFollowUp(interaction, '何も再生してないよ？');
 		}
 	} else if (interaction.commandName === 'resume') {
 		if (subscription) {
 			subscription.audioPlayer.unpause();
-			await interaction.reply({ content: `再生`, ephemeral: true });
+			await usefulReplyOrFollowUp(interaction, { content: `再生`, ephemeral: true });
 		} else {
-			await interaction.reply('何も再生してないよ？');
+			await usefulReplyOrFollowUp(interaction, '何も再生してないよ？');
 		}
 	} else if (interaction.commandName === 'leave') {
 		if (subscription) {
 			subscription.voiceConnection.destroy();
 			guild2subscriptions.delete(interaction.guildId);
-			await interaction.reply({ content: `じゃあの`, ephemeral: true });
+			await usefulReplyOrFollowUp(interaction, { content: `じゃあの`, ephemeral: true });
 		} else {
-			await interaction.reply('いねーよ');
+			await usefulReplyOrFollowUp(interaction, 'いねーよ');
 		}
 	} else if (interaction.commandName === 'ping') {
-		await interaction.reply({ content: 'Pong!', ephemeral: true });
+		await usefulReplyOrFollowUp(interaction, { content: 'Pong!', ephemeral: true });
 	} else {
-		await interaction.reply('知らねーことをいうんじゃねえ。未実装だ');
+		await usefulReplyOrFollowUp(interaction, '知らねーことをいうんじゃねえ。未実装だ');
 	}
 });
 
