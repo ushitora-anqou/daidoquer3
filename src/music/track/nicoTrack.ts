a56c6dff64449f27acfc16b33284203e0a2f8def
plveir <featveir@gmail.com>
2021-11-08 04:44:36 +0900

    add niconico track

---
+++ src/music/track/nicoTrack.ts
@@ -0,0 +1,124 @@
+import { Track, TrackData } from './track';
+import { Readable, PassThrough } from 'stream';
+import { getThumbInfo } from 'niconico-thumbinfo';
+import { URL } from 'node:url';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
+const ffmpeg = require('ffmpeg-static');
+import * as cp from 'child_process';
+
+/**
+ * Track implements for Niconico.
+ */
+export class NicoTrack extends Track {
+  constructor({ url, title, thumbnailUrl, onStart, onFinish, onError }: TrackData) {
+    super({ url, title, thumbnailUrl, onStart, onFinish, onError });
+  }
+
+  public static url2VideoId(url: string): string {
+    const validatedUrl = new URL(url);
+    return validatedUrl.pathname.replace('/watch/', '');
+  }
+
+  public async createStream(): Promise<Readable> {
+    return new Promise((resolve, reject) => {
+      try {
+        const audio: PassThrough = new PassThrough();
+        const ytdlpProcess = cp.spawn(
+          `./vendor/yt-dlp/yt-dlp`,
+          ['--no-check-certificate', '-o', '-', '-q', `${this.url}`],
+          {
+            windowsHide: true,
+            stdio: [
+              /* Standard: stdin, stdout, stderr */
+              'inherit',
+              'pipe',
+              'inherit',
+            ],
+          }
+        );
+        const ffmpegProcess = cp.spawn(
+          ffmpeg,
+          [
+            // Remove ffmpeg's console spamming
+            '-loglevel',
+            '8',
+            '-hide_banner',
+            // set input
+            '-i',
+            'pipe:0',
+            // video no
+            '-vn',
+            // codec
+            '-acodec',
+            'libopus',
+            // muxer
+            // FIXME: setting webm says
+            /**
+             * Did not find the EBML tag at the start of the stream
+             *  at WebmDemuxer._readTag (.../daidoquer3/node_modules/prism-media/src/core/WebmBase.js:113:18)
+             *  at WebmDemuxer._transform (.../daidoquer3/node_modules/prism-media/src/core/WebmBase.js:47:23)
+             */
+            // idk why.
+            '-f',
+            'ogg',
+            // Define output to pipe
+            'pipe:1',
+          ],
+          {
+            windowsHide: true,
+            stdio: [
+              /* Standard: stdin, stdout, stderr */
+              'pipe',
+              'pipe',
+              'inherit',
+            ],
+          }
+        );
+        // ytdlp Link streams
+        ytdlpProcess.stdio[1].on('data', (chunk) => {
+          ffmpegProcess.stdin.write(chunk);
+        });
+        ytdlpProcess.on('error', (e) => {
+          ffmpegProcess.stdin.end();
+          reject(e)
+        });
+        ytdlpProcess.on('close', () => {
+          ffmpegProcess.stdin.end();
+        });
+        // ffmpeg Link streams
+        ffmpegProcess.stdio[1].on('data', (chunk) => {
+          audio.write(chunk);
+        });
+        ffmpegProcess.on('error', (e) => {
+          audio.end();
+          reject(e);
+        });
+        ffmpegProcess.on('close', (code, signal) => {
+          if (code !== 0) {
+            console.warn(code, signal);
+            reject('ffmpeg Error');
+          } else {
+            resolve(audio);
+          }
+        });
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  public static async from(url: string, methods: Pick<Track, 'onStart' | 'onFinish' | 'onError'>): Promise<NicoTrack> {
+    let title: string | undefined;
+    let thumbnailUrl: string | undefined;
+    await getThumbInfo(NicoTrack.url2VideoId(url)).then((info) => {
+      (title = info.avalable ? info.title : undefined), (thumbnailUrl = info.avalable ? info.thumbnailUrl : undefined);
+    });
+    return new NicoTrack({
+      url,
+      title: title,
+      thumbnailUrl: thumbnailUrl,
+      ...this.wrapMethods(methods),
+    });
+  }
+}
--- src/music/url2enqueue.ts
+++ src/music/url2enqueue.ts
@@ -1,87 +1,102 @@
-import { CacheType, CommandInteraction, MessageEmbed } from "discord.js";
-import ytdl from "ytdl-core";
-import ytpl from "ytpl";
-import { MusicSubscription } from "./subscription";
-import { Track } from "./track/track";
-import { YtTrack } from "./track/ytTrack";
-import { URL } from "node:url";
-import { ScTrack } from "./track/scTrack";
-import { usefulReplyOrFollowUp } from "../discord/util";
+import { CacheType, CommandInteraction, MessageEmbed } from 'discord.js';
+import ytdl from 'ytdl-core';
+import ytpl from 'ytpl';
+import { MusicSubscription } from './subscription';
+import { Track } from './track/track';
+import { YtTrack } from './track/ytTrack';
+import { URL } from 'node:url';
+import { ScTrack } from './track/scTrack';
+import { usefulReplyOrFollowUp } from '../discord/util';
+import { NicoTrack } from './track/nicoTrack';
 
 export class Url2Enqueue {
-    // Enqueue the track and reply a success message to the user
-    public static async fromUrl(subscription: MusicSubscription, interaction: CommandInteraction<CacheType>, url: string, methods: Pick<Track, 'onStart' | 'onFinish' | 'onError'>): Promise<void> {
-        const tracks: Track[] = [];
-        const msgEmbed = new MessageEmbed();
+  // Enqueue the track and reply a success message to the user
+  public static async fromUrl(
+    subscription: MusicSubscription,
+    interaction: CommandInteraction<CacheType>,
+    url: string,
+    methods: Pick<Track, 'onStart' | 'onFinish' | 'onError'>
+  ): Promise<void> {
+    const tracks: Track[] = [];
+    const msgEmbed = new MessageEmbed();
+    let representiveTrack: Pick<Track, 'title' | 'thumbnailUrl'> | undefined;
 
-        // detect URL
-        if (ytpl.validateID(url)) {
-            // youtube playlist
-            console.log(url, "is Youtube playList.");
-            const plId = await ytpl.getPlaylistID(url);
-            const list = await ytpl(plId);
+    // detect URL
+    if (ytpl.validateID(url)) {
+      // youtube playlist
+      console.log(url, 'is Youtube playList.');
+      const plId = await ytpl.getPlaylistID(url);
+      const list = await ytpl(plId);
+      representiveTrack = { title: list.title, thumbnailUrl: list.bestThumbnail.url ?? undefined };
 
-            msgEmbed.setTitle(`${list.items.length} items in playlist are enqueued`)
-                .setDescription(`[${list.title}](${url})`)
-                .setThumbnail(list.thumbnails[0]?.url ? list.thumbnails[0]?.url : 'Undefined')
-                .setAuthor("Youtube Playlist")
-                .setColor('#FF0000')
-                
-            list.items.forEach(item => {
-                const track = new YtTrack({
-                    url,
-                    title: item.title,
-                    thumbnailUrl: item.thumbnails[0]?.url ? item.thumbnails[0]?.url : 'Undefined',
-                    ... YtTrack.wrapMethods(methods)
-                })
-                tracks.push(track);
-            })
-        } else if (ytdl.validateURL(url)) {
-            // youtube (should not place this block before ytpl)
-            console.log(url, "is Youtube.");
-            const track = await YtTrack.from(url, methods);
-            tracks.push(track);
-            msgEmbed.setTitle(`Enqueued`)
-                .setDescription(`[${track.title}](${url})`)
-                .setThumbnail(track.thumbnailUrl)
-                .setAuthor("Youtube")
-                .setColor('#FF0000')
-        } else {
-            // URL validate
-            try {
-                const validatedUrl = new URL(url);
-                if (validatedUrl.protocol !== 'http:' && validatedUrl.protocol !== 'https:') {
-                    throw TypeError("Invalid URL")
-                }
-                if (validatedUrl.hostname === 'soundcloud.com') {
-                    console.log(url, "is Soundcloud.");
-                    // soundcloud is too heavy so deferred reply.
-                    await interaction.deferReply();
-                    const track = await ScTrack.from(url, methods);
-                    tracks.push(track);
-                    msgEmbed.setTitle(`Enqueued`)
-                        .setDescription(`[${track.title}](${url})`)
-                        .setThumbnail(track.thumbnailUrl)
-                        .setAuthor("SoundCloud")
-                        .setColor('#FE5000')
-                } else {
-                    throw Error("Unknown URL")
-                }
-            } catch (e) {
-                return new Promise((_, reject) => reject(e));
-            }
-        }
-        // enqueue
-        for(const pt of tracks) {
-            subscription.enqueue(pt);
-            console.log("enqueued");
-        }
-        // message reply
-        await usefulReplyOrFollowUp(interaction, {
-            embeds: [
-                msgEmbed
-            ],
-            ephemeral: false
+      msgEmbed
+        .setTitle(`${list.items.length} items in playlist are enqueued`)
+        .setAuthor('Youtube Playlist')
+        .setColor('#FF0000');
+
+      list.items.forEach((item) => {
+        const track = new YtTrack({
+          url: item.url,
+          title: item.title,
+          thumbnailUrl: item.thumbnails[0]?.url ?? undefined,
+          ...YtTrack.wrapMethods(methods),
         });
+        tracks.push(track);
+      });
+    } else if (ytdl.validateURL(url)) {
+      // youtube (should not place this block before ytpl)
+      console.log(url, 'is Youtube.');
+      const track = await YtTrack.from(url, methods);
+      representiveTrack = { title: track.title, thumbnailUrl: track.thumbnailUrl };
+      tracks.push(track);
+      msgEmbed.setTitle(`Enqueued`).setAuthor('Youtube').setColor('#FF0000');
+    } else {
+      // URL validate
+      try {
+        const validatedUrl = new URL(url);
+        if (validatedUrl.protocol !== 'http:' && validatedUrl.protocol !== 'https:') {
+          throw TypeError('Invalid URL');
+        }
+        if (validatedUrl.hostname === 'soundcloud.com') {
+          console.log(url, 'is Soundcloud.');
+          // soundcloud is too heavy so deferred reply.
+          await interaction.deferReply();
+          const track = await ScTrack.from(url, methods);
+          representiveTrack = { title: track.title, thumbnailUrl: track.thumbnailUrl };
+          tracks.push(track);
+          msgEmbed
+            .setTitle(`Enqueued`)
+            .setAuthor(
+              'SoundCloud',
+              'https://developers.soundcloud.com/assets/logo_big_white-65c2b096da68dd533db18b9f07d14054.png'
+            )
+            .setColor('#FE5000');
+        } else if (validatedUrl.hostname === 'www.nicovideo.jp') {
+          console.log(url, 'is NicoVideo.');
+          const track = await NicoTrack.from(url, methods);
+          representiveTrack = { title: track.title, thumbnailUrl: track.thumbnailUrl };
+          tracks.push(track);
+          msgEmbed.setTitle(`Enqueued`).setAuthor('Niconico').setColor('#252525');
+        } else {
+          throw Error('Unknown URL');
+        }
+      } catch (e) {
+        return new Promise((_, reject) => reject(e));
+      }
     }
+    // enqueue
+    for (const pt of tracks) {
+      subscription.enqueue(pt);
+      console.log('enqueued');
+    }
+    // message reply
+    if (representiveTrack) {
+      if (representiveTrack.title) msgEmbed.setDescription(`[${representiveTrack.title}](${url})`);
+      if (representiveTrack.thumbnailUrl) msgEmbed.setThumbnail(representiveTrack.thumbnailUrl);
+    }
+    await usefulReplyOrFollowUp(interaction, {
+      embeds: [msgEmbed],
+      ephemeral: false,
+    });
+  }
 }
