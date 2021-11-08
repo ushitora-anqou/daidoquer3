5bc6fadce86b9157508e86249ce8b380e4ac948a
plveir <featveir@gmail.com>
2021-11-08 15:51:54 +0900

    add soft lock in reply

--- src/discord/util.ts
+++ src/discord/util.ts
@@ -1,20 +1,24 @@
 import { CacheType, CommandInteraction, InteractionReplyOptions, MessagePayload } from 'discord.js';
 
 export async function usefulReplyOrFollowUp(
-  interaction: CommandInteraction<CacheType>,
+  interaction: CommandInteraction<CacheType> & {repliedSoftLock?: boolean},
   options: string | MessagePayload | InteractionReplyOptions
 ): Promise<void> {
+  let tryingReply = false;
   try {
-    if (interaction.deferred && interaction.replied) {
+    if (interaction.deferred && interaction.replied && interaction.repliedSoftLock != null) {
       await interaction.followUp(options).catch(console.warn);
-    } else if (interaction.replied) {
+    } else if (interaction.replied || interaction.repliedSoftLock) {
       await interaction.followUp(options).catch(console.warn);
     } else if (interaction.deferred) {
       await interaction.editReply(options).catch(console.warn);
     } else {
+      tryingReply = true;
+      interaction.repliedSoftLock = true;
       await interaction.reply(options).catch(console.warn);
     }
   } catch (e) {
+    if (tryingReply) interaction.repliedSoftLock = false;
     console.warn(e);
     console.warn(options.toString);
   }
